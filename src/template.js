15152cae746bd3f33748084d3c3d20c0f1f8037e
LucasBielinski <115428815+LucasBielinski@users.noreply.github.com>
2023-01-24 17:57:33 -0500

    add tests add template

---
+++ src/template.js
@@ -0,0 +1,70 @@
+function renderCard(employee) {
+  var baseCard = `<div class="col-sm-8 col-lg-3">
+    <div class="card">
+    <div class="card-body">
+    <h2 class="card-text"> ${employee.getRole()}</h2>
+    <p class="card-text"> name: ${employee.name} </p>
+    <p class="card-text"> ID: ${employee.id}</p>
+    <p>Emaill: <a class="card-text" href="mailto:${employee.email}"> ${
+    employee.email
+  }</a></p>`;
+
+  switch (employee.getRole()) {
+    case "Manager":
+      baseCard += `<p class="card-text"> Office Number: ${employee.number}</p>`;
+      break;
+    case "Engineer":
+      baseCard += `<a class="card-text" href="${employee.github}" target="_blank" rel="noopener noreferrer"> Employee GitHub </a>`;
+      break;
+    default:
+      baseCard += `<p class="card-text"> School: ${employee.school}</p>`;
+      break;
+  }
+
+  baseCard += ` </div></div></div>`;
+
+  return baseCard;
+}
+
+function template(team) {
+  console.log(team);
+
+  //in order to access them we must access their index team[I]
+  var cards = "";
+  for (let i = 0; i < team.length; i++) {
+    cards += renderCard(team[i]);
+  }
+
+  return `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+    />
+    <link rel="stylesheet" href="./style.css"/>
+      <title>Team Building</title>
+  </head>
+  <body>
+
+  <header class="text-center p-2">
+  <div class="page-header mt-2">
+    <h1>Introducing the Team!</h1>
+  </div>
+</header>
+
+<section class="mt-3 mb-2 container">
+<div class="row">
+${cards}
+
+  </div>
+</section>
+      
+  </body>
+  </html>`;
+}
+
+module.exports = template;
